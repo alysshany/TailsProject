8750d872d9045dbc199672b1a2e8299fcddb0989
Ruslana <garaeva-r_23@mail.ru>
2023-04-01 09:48:53 +0300

    :(

--- lib/home.dart
+++ lib/home.dart
@@ -12,48 +12,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         title: Image.asset("images/cat-whiskers.png"),
         centerTitle: true,
       ),
       body: Column(
-        children: <Widget> [
+        children: <Widget>[
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.18,
           ),
           Container(
             alignment: Alignment.topCenter,
-            child: ClipRRect
-            (
+            child: ClipRRect(
               child: DropShadow(
                 blurRadius: 20,
                 borderRadius: 15,
                 child: Image.asset("images/image2.png"),
-                ),
               ),
             ),
+          ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.02,
           ),
           Container(
-            width: MediaQuery.of(context).size.height * 0.40,
-            alignment: Alignment.topCenter,
-            child: const Text(
-              "А знаете ли вы, как приятно просыпаться не оттого, что тишину пронзительно взрывает будильник, а оттого, что кошка мурлыкает над ухом, поет свою кошачью песенку...",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontFamily: 'PlayfairDisplay'),
-              )
-            
-            ),
-        ],),
+              width: MediaQuery.of(context).size.height * 0.40,
+              alignment: Alignment.topCenter,
+              child: const Text(
+                "А знаете ли вы, как приятно просыпаться не оттого, что тишину пронзительно взрывает будильник, а оттого, что кошка мурлыкает над ухом, поет свою кошачью песенку...",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'PlayfairDisplay'),
+              )),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Add your onPressed code here!
+          Navigator.pushNamed(context, '/auth');
         },
-        backgroundColor: Color.fromARGB(199, 123, 189, 255),
+        backgroundColor: const Color.fromARGB(199, 123, 189, 255),
         child: const Icon(Icons.navigate_next),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:tails_app/auth/auth.dart';
 import 'package:tails_app/home.dart';
 
 void main() {
@@ -16,22 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-          '/home': (context) => const HomePage(),
-        } ,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //Splash screen необходим для первой загрузочной страницы
-      //Взят с pub dev'а
+        '/home': (context) => const HomePage(),
+        '/auth': (context) => const AuthPage(),
+      },
       home: AnimatedSplashScreen(
-            duration: 3000,
-            splash: "images/cat-whiskers.png",
-            nextScreen: const HomePage(),
-            splashTransition: SplashTransition.sizeTransition,
-            pageTransitionType: PageTransitionType.leftToRight,
-            backgroundColor: const Color.fromARGB(255, 241, 251, 255)),
+        duration: 3000,
+        splash: "images/cat-whiskers.png",
+        nextScreen: const HomePage(),
+        splashTransition: SplashTransition.sizeTransition,
+        pageTransitionType: PageTransitionType.leftToRight,
+        backgroundColor: const Color.fromARGB(255, 241, 251, 255),
+      ),
     );
   }
 }
-
